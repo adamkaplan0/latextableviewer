d19a2ec7e573e309c60cfd98d0470efd3f955bb2
Adam Kaplan <49291393+adamkaplan0@users.noreply.github.com>
2022-07-13 15:08:08 -0400

    Remove "Download" Button Fixes #4

--- src/Preview.jsx
+++ src/Preview.jsx
@@ -4,8 +4,9 @@ import loadingAnimation from './loading.gif';
 export const Preview = ({compile, pdfUrl, enginesInitialized}) => {
   return (
     <article className="flex flex-col items-left">
-      <div>
-      <button onClick={compile}>Compile</button><button>Download</button><EngineIndicator enginesInitialized={enginesInitialized} />
+      <div className="flex">
+      <EngineIndicator enginesInitialized={enginesInitialized} />
+      <button onClick={compile}>Compile</button>
       </div>
       {pdfUrl !== "" && <embed src={pdfUrl} width="100%" height="1000px" type="application/pdf"></embed>}
       {pdfUrl === "" && <img height="1000px" src={loadingAnimation} />}
