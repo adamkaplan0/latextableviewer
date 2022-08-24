c63e5986f391119adcb4397b5146f8e89fef08a9
Adam Kaplan <49291393+adamkaplan0@users.noreply.github.com>
2022-08-24 16:44:08 -0400

    Created more consistent design cues across the app

--- src/features/header/Header.jsx
+++ src/features/header/Header.jsx
@@ -4,7 +4,7 @@ import { Link, NavLink } from "react-router-dom";
 export const Header = () => {
   return (
     <header className="flex justify-between items-center mb-4">
-      <span className="inline-block font-bold text-4xl"><Link to="/">LaTeX Table Viewer</Link></span>
+      <h1 className="inline-block font-bold text-4xl"><Link to="/">LaTeX Table Viewer</Link></h1>
       <nav>
         <ul className="list-none inline-flex justify-around items-center gap-12 text-xl text-center">
           <li><NavLink to="/">App</NavLink></li>
