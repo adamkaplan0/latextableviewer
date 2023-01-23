12b489a6b0198db1186eda62cf2176a3e4644032
Adam Kaplan <49291393+adamkaplan0@users.noreply.github.com>
2023-01-23 15:08:16 -0500

    [Remove] Height for split pane

--- src/components/SplitPane.jsx
+++ src/components/SplitPane.jsx
@@ -1,9 +1,8 @@
 export const SplitPane = ({children}) => {
   // Split the pane in half and add the children to it
   return (
-    <article className="grid grid-cols-2 gap-4 items-left place-content-stretch h-full">
+    <article className="grid grid-cols-2 gap-4 items-left place-content-stretch">
       {children}
     </article>
   )
 };
-// "grid grid-cols-2 gap-4 items-left place-content-stretch h-full"
